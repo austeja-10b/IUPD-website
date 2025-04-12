5dfbc8c4228be6d1ca293eef2a8d35eb75b8099d
austeja10b <austeja.tereikyte@gmail.com>
2025-04-12 16:07:20 +0300

    Add files via upload

---
+++ scripty_eng.js
@@ -0,0 +1,76 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+    document.body.classList.toggle("show-mobile-menu");
+});
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        if (document.body.classList.contains("show-mobile-menu")) {
+            document.body.classList.remove("show-mobile-menu");
+        }
+    });
+});
+
+menuCloseButton.addEventListener("click", () => {
+    document.body.classList.remove("show-mobile-menu");
+});
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.querySelectorAll(".more-info");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", function() {
+            let info = button.nextElementSibling;
+
+            if (info.style.display === "block") {
+                info.style.display = "none";
+                button.textContent = "More info";
+            } else {
+                info.style.display = "block";
+                button.textContent = "Less info";
+            }
+        });
+    });
+});
+
+
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 25,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // Responsive breakpoint
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+});
+
+
